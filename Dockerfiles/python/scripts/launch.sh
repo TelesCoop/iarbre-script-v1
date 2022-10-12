28b4785e95f3d8bd33e462dc22ae5f523ff7c6ae
Pierre-Gilles Levallois <pierregilles.levallois@gmail.com>
2022-10-12 10:49:07 +0200

    launch all commands

--- Dockerfiles/python/scripts/launch.sh
+++ Dockerfiles/python/scripts/launch.sh
@@ -1,1 +1,10 @@
-screen -dmS calque_metro python3 main.py computeAll 5
+#!/bin/bash
+
+# screen -dmS calque_metro python3 main.py computeAll 5
+
+python3 main.py initCommunes
+# python3 main.py initGrid
+# python3 main.py initDatas
+# python3 main.py computeFactors
+# python3 main.py computeIndices
+# python3 main.py computeAll
