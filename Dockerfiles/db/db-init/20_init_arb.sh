2f22c50fa268bea4193643ed2d4e1673f169ac36
Pierre-Gilles Levallois <pierregilles.levallois@gmail.com>
2022-10-12 11:22:05 +0200

    Add sql scripts here

--- Dockerfiles/db/db-init/20_init_arb.sh
+++ Dockerfiles/db/db-init/20_init_arb.sh
@@ -16,9 +16,9 @@ echo "POSTGRES_PORT=$POSTGRES_PORT"
 echo "POSTGRES_SERVER=$POSTGRES_SERVER"     
 echo "POSTGRES_USER=$POSTGRES_USER"       
 echo "POSTGRES_SCHEMA=$POSTGRES_SCHEMA"  
-# psql -U $POSTGRES_USER -d $POSTGRES_DB -c 'CREATE ROLE postgres'
-# psql -U $POSTGRES_USER -d $POSTGRES_DB -f /docker-entrypoint-initdb.d/backups/BACKUP_SCHEMA.sql
-# psql -U $POSTGRES_USER -d $POSTGRES_DB -c 'ALTER TABLE prod_immo.operation ADD etat_chargement varchar NULL;'
+psql -U $POSTGRES_USER -d $POSTGRES_DB -f /docker-entrypoint-initdb.d/10-copy_base.sql
+psql -U $POSTGRES_USER -d $POSTGRES_DB -f /docker-entrypoint-initdb.d/create_tables.sql
+psql -U $POSTGRES_USER -d $POSTGRES_DB -f /docker-entrypoint-initdb.d/insert_data.sql
 echo "--------------------------------------------------------------------------"
 echo "     Base de donnees ARB prete."
 echo "--------------------------------------------------------------------------"
