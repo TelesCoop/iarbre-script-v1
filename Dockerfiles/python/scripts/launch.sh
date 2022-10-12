8f4dc1069a7a6f7c220facfb3580e806a5fafb91
Pierre-Gilles Levallois <pierregilles.levallois@gmail.com>
2022-10-12 17:49:53 +0200

    Add a pvc to job

--- Dockerfiles/python/scripts/launch.sh
+++ Dockerfiles/python/scripts/launch.sh
@@ -3,8 +3,14 @@
 # screen -dmS calque_metro python3 main.py computeAll 5
 
 python3 main.py initCommunes
+
 # python3 main.py initGrid
 # python3 main.py initDatas
+
+# Multiprocessing task
 # python3 main.py computeFactors
+
 # python3 main.py computeIndices
+
+# Launching everything, it is possible to give a list of township
 # python3 main.py computeAll
