a081fe2ad5efa47d4649802f226812b889b63885
Pierre-Gilles Levallois <pierregilles.levallois@gmail.com>
2022-10-13 14:00:11 +0200

    set persistent volume claim

--- Dockerfiles/python/scripts/launch.sh
+++ Dockerfiles/python/scripts/launch.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
+# ARB : Lancement du recalcul des indices du calque de plantabilité.
+#
 # screen -dmS calque_metro python3 main.py computeAll 5
 
 python3 main.py initCommunes
+
+sleep 300
 
 # python3 main.py initGrid
 # python3 main.py initDatas
