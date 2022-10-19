b979e9997ea6e31ba182cdf30c41d82cfd6a8bee
Pierre-Gilles Levallois <pierregilles.levallois@gmail.com>
2022-10-19 14:32:50 +0200

    Update _data script as Imgae entrypoint

--- Dockerfiles/git/update_data.sh
+++ Dockerfiles/git/update_data.sh
@@ -2,6 +2,10 @@
 
 mkdir -p /arb-data/source-files/data-recalcul-calque
 cd /arb-data/source-files/data-recalcul-calque
+
+# Init command does not do a 'git clone' beacause this is only working at the first init time.
+# It does a 'git init + git remote add + git fetch + git checkout' instead.
+# This works in every case.
 [[ ! -d ".git" ]] && { 
     git init
     git remote add origin https://${GIT_USERNAME}:${GIT_PASSWORD}@forge.grandlyon.com/erasme/sources-recalcul-calque.git
