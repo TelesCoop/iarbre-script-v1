69f3eced4d31015e387c403b09a2f6c2927cc851
Pierre-Gilles Levallois <pierregilles.levallois@gmail.com>
2022-12-01 11:33:20 +0100

    dumping communes to a file

--- Dockerfiles/python/scripts/utils.py
+++ Dockerfiles/python/scripts/utils.py
@@ -370,6 +370,13 @@ def insertGDFintoDB(DB_params, DB_schema, gdf, tablename, columnsListToDB):
     gdf.to_csv(buffer, sep=';', index_label='id', header=False, index=False)
     buffer.seek(0)
 
+    #  PGL - Debug
+    import shutil
+    with open('/app/tmp/dump_communes_gl.txt', 'w') as fd:
+        shutil.copyfileobj(buffer, fd)
+        buffer.seek(0)
+    # /PGL - Debug
+
     # Set Schema 'base'
     cur.execute(f'SET search_path TO ' + DB_schema)
 
