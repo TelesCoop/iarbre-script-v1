1df616a0a282183b9f089a28a4feabd4d737178b
Pierre-Gilles Levallois <pierregilles.levallois@gmail.com>
2022-12-06 10:21:17 +0100

    launch computation on one township only

--- Dockerfiles/python/scripts/launch.sh
+++ Dockerfiles/python/scripts/launch.sh
@@ -2,11 +2,85 @@
 ################################################################################
 # ARB : Lancement du recalcul des indices du calque de plantabilité.
 ################################################################################
+
+# Liste des commmunes : 
+# ----+----------------------------+-------
+#  id |          libelle           | insee 
+# ----+----------------------------+-------
+#   1 | LYON 7EME                  | 69387
+#   2 | LYON 8EME                  | 69388
+#   3 | LYON 6EME                  | 69386
+#   4 | LYON 4EME                  | 69384
+#   5 | LYON 9EME                  | 69389
+#   6 | LYON 1ER                   | 69381
+#   7 | LYON 3EME                  | 69383
+#   8 | LYON 5EME                  | 69385
+#   9 | LYON 2EME                  | 69382
+#  10 | FONTAINES-SAINT-MARTIN     | 69087
+#  11 | JONAGE                     | 69279
+#  12 | CAILLOUX-SUR-FONTAINES     | 69033
+#  13 | VERNAISON                  | 69260
+#  14 | SAINT-GERMAIN-AU-MONT-D'OR | 69207
+#  15 | DECINES-CHARPIEU           | 69275  <-
+#  16 | GENAY                      | 69278
+#  17 | VILLEURBANNE               | 69266
+#  18 | VENISSIEUX                 | 69259
+#  19 | FONTAINES-SUR-SAONE        | 69088
+#  20 | IRIGNY                     | 69100
+#  21 | SAINT-GENIS-LAVAL          | 69204
+#  22 | SATHONAY-VILLAGE           | 69293
+#  23 | GIVORS                     | 69091
+#  24 | GRIGNY                     | 69096
+#  25 | LISSIEU                    | 69117
+#  26 | CHASSIEU                   | 69271
+#  27 | CHARLY                     | 69046
+#  28 | VAULX-EN-VELIN             | 69256
+#  29 | SAINT-PRIEST               | 69290
+#  30 | SOLAIZE                    | 69296
+#  31 | SAINT-FONS                 | 69199
+#  32 | MIONS                      | 69283
+#  33 | SATHONAY-CAMP              | 69292
+#  34 | FEYZIN                     | 69276
+#  35 | OULLINS                    | 69149
+#  36 | MEYZIEU                    | 69282
+#  37 | CHAMPAGNE-AU-MONT-D'OR     | 69040
+#  38 | ECULLY                     | 69081
+#  39 | SAINT-ROMAIN-AU-MONT-D'OR  | 69233
+#  40 | SAINT-CYR-AU-MONT-D'OR     | 69191
+#  41 | COLLONGES-AU-MONT-D'OR     | 69063
+#  42 | CRAPONNE                   | 69069
+#  43 | POLEYMIEUX-AU-MONT-D'OR    | 69153
+#  44 | LIMONEST                   | 69116
+#  45 | SAINT-GENIS-LES-OLLIERES   | 69205
+#  46 | SAINT-DIDIER-AU-MONT-D'OR  | 69194
+#  47 | DARDILLY                   | 69072
+#  48 | FRANCHEVILLE               | 69089
+#  49 | SAINTE-FOY-LES-LYON        | 69202
+#  50 | MARCY-L'ETOILE             | 69127
+#  51 | BRON                       | 69029
+#  52 | QUINCIEUX                  | 69163
+#  53 | RILLIEUX-LA-PAPE           | 69286
+#  54 | CORBAS                     | 69273
+#  55 | PIERRE-BENITE              | 69152
+#  56 | COUZON-AU-MONT-D'OR        | 69068
+#  57 | ALBIGNY-SUR-SAONE          | 69003
+#  58 | LA TOUR-DE-SALVAGNY        | 69250
+#  59 | TASSIN-LA-DEMI-LUNE        | 69244
+#  60 | CHARBONNIERES-LES-BAINS    | 69044
+#  61 | FLEURIEU-SUR-SAONE         | 69085
+#  62 | LA MULATIERE               | 69142
+#  63 | NEUVILLE-SUR-SAONE         | 69143
+#  64 | CURIS-AU-MONT-D'OR         | 69071
+#  65 | ROCHETAILLEE-SUR-SAONE     | 69168
+#  66 | MONTANAY                   | 69284
+#  67 | CALUIRE-ET-CUIRE           | 69034
+
 namespace_env=$1
 DB_HOST=$2
 DB_PORT=$3
 DB_NAME=$4
 DB_USER=$5
+CODE_INSEE=69275
 
 DATA_REPO="https://forge.grandlyon.com/erasme/data-recalcul-calque.git"
 
@@ -83,20 +157,20 @@ check
 # check
 
 comment "Init Grid"
-python3 main.py initGrid 5
+python3 main.py initGrid 5 $CODE_INSEE
 check
 
 comment "InitDatas"
-python3 main.py initDatas
+python3 main.py initDatas $CODE_INSEE
 check
 
 stage "Launch Computations..."
 comment "Computing factors"
-python3 main.py computeFactors # Possibly Multiprocessing task, Should have a list of townships
+python3 main.py computeFactors $CODE_INSEE # Possibly Multiprocessing task, Should have a list of townships
 check 
 
 comment "Computing Indices"
-python3 main.py computeIndices
+python3 main.py computeIndices $CODE_INSEE 
 check
 
 # Launching everything, it is possible to give a list of townships
