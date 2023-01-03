bd8366e34221a2750a5617c561bdfecc230ae8ba
Pierre-Gilles Levallois <pierregilles.levallois@gmail.com>
2023-01-03 16:12:00 +0100

    raise grid to 50

--- Dockerfiles/python/scripts/launch.sh
+++ Dockerfiles/python/scripts/launch.sh
@@ -2,79 +2,6 @@
 ################################################################################
 # ARB : Lancement du recalcul des indices du calque de plantabilité.
 ################################################################################
-
-# Liste des commmunes : 
-# ----+----------------------------+-------
-#  id |          libelle           | insee 
-# ----+----------------------------+-------
-#   1 | LYON 7EME                  | 69387
-#   2 | LYON 8EME                  | 69388
-#   3 | LYON 6EME                  | 69386
-#   4 | LYON 4EME                  | 69384
-#   5 | LYON 9EME                  | 69389
-#   6 | LYON 1ER                   | 69381
-#   7 | LYON 3EME                  | 69383
-#   8 | LYON 5EME                  | 69385
-#   9 | LYON 2EME                  | 69382
-#  10 | FONTAINES-SAINT-MARTIN     | 69087
-#  11 | JONAGE                     | 69279
-#  12 | CAILLOUX-SUR-FONTAINES     | 69033
-#  13 | VERNAISON                  | 69260
-#  14 | SAINT-GERMAIN-AU-MONT-D'OR | 69207
-#  15 | DECINES-CHARPIEU           | 69275  <-
-#  16 | GENAY                      | 69278
-#  17 | VILLEURBANNE               | 69266
-#  18 | VENISSIEUX                 | 69259
-#  19 | FONTAINES-SUR-SAONE        | 69088
-#  20 | IRIGNY                     | 69100
-#  21 | SAINT-GENIS-LAVAL          | 69204
-#  22 | SATHONAY-VILLAGE           | 69293
-#  23 | GIVORS                     | 69091
-#  24 | GRIGNY                     | 69096
-#  25 | LISSIEU                    | 69117
-#  26 | CHASSIEU                   | 69271
-#  27 | CHARLY                     | 69046
-#  28 | VAULX-EN-VELIN             | 69256
-#  29 | SAINT-PRIEST               | 69290
-#  30 | SOLAIZE                    | 69296
-#  31 | SAINT-FONS                 | 69199
-#  32 | MIONS                      | 69283
-#  33 | SATHONAY-CAMP              | 69292
-#  34 | FEYZIN                     | 69276
-#  35 | OULLINS                    | 69149
-#  36 | MEYZIEU                    | 69282
-#  37 | CHAMPAGNE-AU-MONT-D'OR     | 69040
-#  38 | ECULLY                     | 69081
-#  39 | SAINT-ROMAIN-AU-MONT-D'OR  | 69233
-#  40 | SAINT-CYR-AU-MONT-D'OR     | 69191
-#  41 | COLLONGES-AU-MONT-D'OR     | 69063
-#  42 | CRAPONNE                   | 69069
-#  43 | POLEYMIEUX-AU-MONT-D'OR    | 69153
-#  44 | LIMONEST                   | 69116
-#  45 | SAINT-GENIS-LES-OLLIERES   | 69205
-#  46 | SAINT-DIDIER-AU-MONT-D'OR  | 69194
-#  47 | DARDILLY                   | 69072
-#  48 | FRANCHEVILLE               | 69089
-#  49 | SAINTE-FOY-LES-LYON        | 69202
-#  50 | MARCY-L'ETOILE             | 69127
-#  51 | BRON                       | 69029
-#  52 | QUINCIEUX                  | 69163
-#  53 | RILLIEUX-LA-PAPE           | 69286
-#  54 | CORBAS                     | 69273
-#  55 | PIERRE-BENITE              | 69152
-#  56 | COUZON-AU-MONT-D'OR        | 69068
-#  57 | ALBIGNY-SUR-SAONE          | 69003
-#  58 | LA TOUR-DE-SALVAGNY        | 69250
-#  59 | TASSIN-LA-DEMI-LUNE        | 69244
-#  60 | CHARBONNIERES-LES-BAINS    | 69044
-#  61 | FLEURIEU-SUR-SAONE         | 69085
-#  62 | LA MULATIERE               | 69142
-#  63 | NEUVILLE-SUR-SAONE         | 69143
-#  64 | CURIS-AU-MONT-D'OR         | 69071
-#  65 | ROCHETAILLEE-SUR-SAONE     | 69168
-#  66 | MONTANAY                   | 69284
-#  67 | CALUIRE-ET-CUIRE           | 69034
-
 alias python=python3
 
 namespace_env=$1
@@ -83,7 +10,7 @@ DB_PORT=$3
 DB_NAME=$4
 DB_USER=$5
 
-GRID_SIZE=25
+GRID_SIZE=50
 
 declare -A LISTE_COMMUNES
 # LISTE_COMMUNES=( ["LYON-7EME"]="69387" ["LYON-8EME"]="69388" ["LYON-6EME"]="69386" ["LYON-4EME"]="69384" ["LYON-9EME"]="69389" ["LYON-1ER"]="69381" ["LYON-3EME"]="69383" ["LYON-5EME"]="69385" ["LYON-2EME"]="69382" ["FONTAINES-SAINT-MARTIN"]="69087" ["JONAGE"]="69279" ["CAILLOUX-SUR-FONTAINES"]="69033" ["VERNAISON"]="69260" ["SAINT-GERMAIN-AU-MONT-D'OR"]="69207" ["DECINES-CHARPIEU"]="69275" ["GENAY"]="69278" ["VILLEURBANNE"]="69266" ["VENISSIEUX"]="69259" ["FONTAINES-SUR-SAONE"]="69088" ["IRIGNY"]="69100" ["SAINT-GENIS-LAVAL"]="69204" ["SATHONAY-VILLAGE"]="69293" ["GIVORS"]="69091" ["GRIGNY"]="69096" ["LISSIEU"]="69117" ["CHASSIEU"]="69271" ["CHARLY"]="69046" ["VAULX-EN-VELIN"]="69256" ["SAINT-PRIEST"]="69290" ["SOLAIZE"]="69296" ["SAINT-FONS"]="69199" ["MIONS"]="69283" ["SATHONAY-CAMP"]="69292" ["FEYZIN"]="69276" ["OULLINS"]="69149" ["MEYZIEU"]="69282" ["CHAMPAGNE-AU-MONT-D'OR"]="69040" ["ECULLY"]="69081" ["SAINT-ROMAIN-AU-MONT-D'OR"]="69233" ["SAINT-CYR-AU-MONT-D'OR"]="69191" ["COLLONGES-AU-MONT-D'OR"]="69063" ["CRAPONNE"]="69069" ["POLEYMIEUX-AU-MONT-D'OR"]="69153" ["LIMONEST"]="69116" ["SAINT-GENIS-LES-OLLIERES"]="69205" ["SAINT-DIDIER-AU-MONT-D'OR"]="69194" ["DARDILLY"]="69072" ["FRANCHEVILLE"]="69089" ["SAINTE-FOY-LESLYON"]="69202" ["MARCY-L'ETOILE"]="69127" ["BRON"]="69029" ["QUINCIEUX"]="69163" ["RILLIEUX-LA-PAPE"]="69286" ["CORBAS"]="69273" ["PIERRE-BENITE"]="69152" ["COUZON-AU-MONT-D'OR"]="69068" ["ALBIGNY-SUR-SAONE"]="69003" ["LA-TOUR-DE-SALVAGNY"]="69250" ["TASSIN-LA-DEMI-LUNE"]="69244" ["CHARBONNIERES-LES-BAINS"]="69044" ["FLEURIEU-SUR-SAONE"]="69085" ["LA-MULATIERE"]="69142" ["NEUVILLE-SUR-SAONE"]="69143" ["CURIS-AU-MONT-D'OR"]="69071" ["ROCHETAILLEE-SUR-SAONE"]="69168" ["MONTANAY"]="69284" ["CALUIRE-ET-CUIRE"]="69034" )
